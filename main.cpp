c046b913638856ce1fcfc48611ab82b7a6be3bfc
jwtkms <limsyman59@gmail.com>
2024-01-19 10:10:11 +0300

    Added new feature
    Now also computes log of a

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int my_power(int a, int b)
     return pow(a, b);
 }
 
+float my_log(int a)
+{
+    return log(a);
+}
+
 int main()
 {
     int a, b;
@@ -14,5 +19,7 @@ int main()
     std::cout << "\nEnter power ";
     std::cin >> b;
     std::cout << a << " in power of " << b << " is " << my_power(a, b) << std::endl;
+    std::cout << "Sqrt of " << a << " in power of " << b << " is " << my_log(a) << std::endl;
+
     return 0;
 }
